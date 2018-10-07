416295839756262168a3a75e86efeb1f04a1e6c4
RapidfireCRH <Rapidfire882007@live.com>
2018-10-07 01:43:42 -0600

    Init

--- NetMsg/Form1.Designer.cs
+++ NetMsg/Form1.Designer.cs
@@ -29,12 +29,94 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 32);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(248, 161);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 199);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(202, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Remote IP";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(271, 230);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- NetMsg/Form1.cs
+++ NetMsg/Form1.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,92 @@ namespace NetMsg
 {
     public partial class Form1 : Form
     {
+        public IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.1"), 8123);
+        public static string[] disp = new string[12];
+        static bool changed = false;
+        UdpClient client = new UdpClient(8123);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+            client.Close();
+            try
+            {
+                remoteIPEndPoint = new IPEndPoint(IPAddress.Parse(textBox1.Text), 8123);
+            }
+            catch
+            {
+                remoteIPEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.1"), 8123);
+                textBox1.Text = "192.168.1.1";
+            }
+            client = new UdpClient(8123);
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            textBox1.Text = "192.168.1.1";
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Send(Encoding.ASCII.GetBytes(textBox3.Text), Encoding.ASCII.GetBytes(textBox3.Text).Length, remoteIPEndPoint);
+            display("Local: " + textBox3.Text);
+            textBox3.Text = "";
+        }
+        static void display(string nextline)
+        {
+            int place = 0;
+            for (; place != disp.Length; place++)
+            {
+                if (disp[place] == null)
+                {
+                    disp[place] = nextline;
+                    changed = true;
+                    return;
+                }
+            }
+            if (disp[--place] != nextline)
+            {
+                for (int i = 1; i != disp.Length; i++)
+                    disp[i - 1] = disp[i];
+                disp[place] = nextline;
+            }
+            changed = true;
+        }
+        public void draw()
+        {
+            foreach (string x in disp)
+                textBox2.Text += x + Environment.NewLine;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!backgroundWorker1.CancellationPending)
+            {
+                Byte[] receivedBytes = client.Receive(ref remoteIPEndPoint);
+                if (receivedBytes == null || receivedBytes.Length == 0)
+                    return;
+                display("Remote: " + Encoding.ASCII.GetString(receivedBytes));
+            }
+            backgroundWorker1.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!changed)
+                return;
+            textBox2.Text = "";
+            foreach (string x in disp)
+                textBox2.Text += x + Environment.NewLine;
+            changed = false;
         }
     }
 }
